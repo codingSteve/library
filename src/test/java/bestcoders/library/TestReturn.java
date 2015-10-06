8047eede5ec2ac6d239ccd59547a4a9f1497b410
Steven Powell <steven.powell@bestcoders.co.uk>
2015-10-06 20:08:53 +0100

    Add test cases for multiple loans to the same member

--- src/test/java/bestcoders/library/TestReturn.java
+++ src/test/java/bestcoders/library/TestReturn.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import bestcoders.library.frontdesk.FrontDesk;
 import bestcoders.library.frontdesk.SimpleFrontDesk;
@@ -17,6 +19,7 @@ import bestcoders.library.items.ItemType;
 import bestcoders.library.members.LibraryMember;
 
 public class TestReturn {
+    private static Logger logger = LoggerFactory.getLogger(TestReturn.class);
 
     @Test
     public void testReturn() {
@@ -50,6 +53,76 @@ public class TestReturn {
 	final FrontDesk d = new SimpleFrontDesk(l);
 
 	final Optional<Item> b = d.findItemById(1);
+	assertTrue(b.isPresent());
+	final Item book = b.get();
+
+	final boolean actualResult = d.returnItem(m, book);
+	final boolean expectedResult = false;
+
+	assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void testReturnWithTwoLoans() {
+	final List<ItemType> types = Arrays.asList(new ItemType[] { ItemType.BOOK });
+
+	final Library library = LibraryFactory.getLibrary();
+
+	final LibraryMember m = new LibraryMember(1, "Steve", types);
+	final FrontDesk d = new SimpleFrontDesk(library);
+
+	for (int i = 0; ++i < 3;) {
+	    logger.info("About to request checkout of item id == {}", i);
+	    final Optional<Item> b = d.findItemById(1);
+	    assertTrue(b.isPresent());
+
+	    final Item book = b.get();
+	    logger.info("Found item == {}", book);
+
+	    final boolean actualResult = d.requestCheckout(m, book);
+	    final boolean expectedResult = true;
+
+	    assertEquals(expectedResult, actualResult);
+
+	}
+
+	final Optional<Item> b = d.findItemById(1);
+	assertTrue(b.isPresent());
+	final Item book = b.get();
+
+	final boolean actualResult = d.returnItem(m, book);
+	final boolean expectedResult = true;
+
+	assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void testReturnWrongBookWithTwoLoans() {
+	final List<ItemType> types = Arrays.asList(new ItemType[] { ItemType.BOOK });
+
+	final Library library = LibraryFactory.getLibrary();
+
+	final LibraryMember m = new LibraryMember(1, "Steve", types);
+	final FrontDesk d = new SimpleFrontDesk(library);
+
+	for (int i = 0; ++i < 3;) {
+	    logger.info("About to request checkout of item id == {}", i);
+	    final Optional<Item> b = d.findItemById(1);
+	    assertTrue(b.isPresent());
+
+	    final Item book = b.get();
+	    logger.info("Found item == {}", book);
+
+	    final boolean actualResult = d.requestCheckout(m, book);
+	    final boolean expectedResult = true;
+
+	    assertEquals(expectedResult, actualResult);
+
+	}
+
+	final Optional<Item> b = d.findItemById(10);
 	assertTrue(b.isPresent());
 	final Item book = b.get();
 
